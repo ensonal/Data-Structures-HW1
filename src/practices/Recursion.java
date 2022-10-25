397e7f44ca14aa108350a919e8c9a31459f96f12
enesonal <enes.onal777@gmail.com>
2022-10-25 03:51:21 +0300

    Practices for recursion algorithms.

--- src/practices/Recursion.java
+++ src/practices/Recursion.java
@@ -58,7 +58,7 @@ class Recursion{
             return data[low];
         }else{
             int mid = (low+high) / 2;
-            return binarySum(data, low, mid) + binarySum(data, mid+1, high) 
+            return binarySum(data, low, mid) + binarySum(data, mid+1, high);
         }
     }
 
